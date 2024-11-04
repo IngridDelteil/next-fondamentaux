38dc7a3138699f75113ec1b9825ec9234f1b294b
Ingrid Delteil <ingrid.delteil@gmail.com>
2024-11-04 22:02:57 +0100

    Exercice et Bonus : add client directive and alert on click in exercise page

--- src/app/exercise/about/page.exercise.tsx
+++ src/app/exercise/about/page.exercise.tsx
@@ -1,12 +1,13 @@
+"use client"
 const Page = () => {
   // 🐶 Détermine dans quel environnement s'exécute ce composant grâce à un log
   // 🤖 `console.log('Je suis un développeur FullStack Next')`
-  //
+  console.log("Je suis un développeur FullStack Next")
   // 🐶 Vérifie dans la console du navigateur et dans la console du serveur (terminal)
 
   // 🐶 N'oublie pas de faire les exercices bonus
   return (
-    <div>
+    <div onClick={() => alert("Hello")}>
       <p>Je suis un développeur FullStack Next</p>
     </div>
   )
