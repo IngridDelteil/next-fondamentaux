98c7ac86ed750b27e4c56a883cc5dbc9cfe93cf3
Ingrid Delteil <ingrid.delteil@gmail.com>
2024-11-05 15:13:36 +0100

    Exercice et Bonus fait : refactor layout components and add header/footer structure

--- src/app/exercise/account/profile/layout.exercise.tsx
+++ src/app/exercise/account/profile/layout.exercise.tsx
@@ -1,13 +1,14 @@
-import React from 'react'
+import React from "react"
 
-export default function Layout({children}: {children: React.ReactNode}) {
+export default function Layout({ children }: { children: React.ReactNode }) {
   // 🐶 Le Layout ne fait rien pour le moment que transmettre le children
   return (
     <div
-    // 🤖 Décommente le style (tailwind) ci-dessous pour avoir une zone orange
-    // className="flex h-[200px] w-[400px] flex-col items-center justify-center border-4 border-yellow-700 bg-yellow-500 font-bold text-white"
+      // 🤖 Décommente le style (tailwind) ci-dessous pour avoir une zone orange
+      className="flex h-[200px] w-[400px] flex-col items-center justify-center border-4 border-yellow-700 bg-yellow-500 font-bold text-white"
     >
       {/* 🐶 Ajoute le titre 'Zone Danger' */}
+      <h1>Zone Danger</h1>
       {children}
     </div>
   )
