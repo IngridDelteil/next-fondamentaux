cee0818ab602da51f40cdbc3b357a127b56ff1cf
Ingrid Delteil <ingrid.delteil@gmail.com>
2024-11-06 23:07:17 +0100

    Exercice et bonus faits : enhance styling and layout for exercise page

--- src/app/exercise/about/page.exercise.tsx
+++ src/app/exercise/about/page.exercise.tsx
@@ -1,12 +1,28 @@
 import Link from 'next/link'
+import styles from './page.module.css'
+import clsx from 'clsx'
 
 const Page = () => {
+  const isAdmin = Math.random() > 0.5
   return (
     <div>
       {/* 🐶 Applique le style global `.title` sur l'élément <p> */}
-      <p>Je suis un développeur FullStack Next</p>
+      <p
+        className={clsx('inline-flex items-center', {
+          'text-red-500': isAdmin,
+        })}
+      >
+        Je suis un développeur FullStack Next
+      </p>
       {/* 🐶 Applique le style `tailwind` sur le composant `<Link>` pour le transformer en bouton*/}
-      <Link href="/exercise/account">Account</Link>
+      <div className="mt-4">
+        <Link
+          href="/exercise/account"
+          className="rounded border border-gray-400 bg-white px-4 py-2 font-bold text-gray-800 hover:bg-gray-100"
+        >
+          Account
+        </Link>
+      </div>
       {/* 🐶 Tu peux wrapper d'une `div` avec 'mt-4' pour ajouter un margin top sur le button*/}
 
       {/* 🐶 Pense à regarder l'exercice bonus*/}
